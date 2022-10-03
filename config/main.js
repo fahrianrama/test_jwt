473a35b7e903e07d4587a568a54286a5d7fbfaf2
fahrianrama <fahrianramaditiya7@gmail.com>
2022-10-03 10:03:42 +0700

    update db

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 module.exports = {
     'secret': "Passphrase for encryption",
-    'database_host': 'localhost',
-    'database_user': 'root',
-    'database_password': '',
-    'database_name': 'test_jwt'
+    'database_host': 'sql6.freesqldatabase.com',
+    'database_user': 'sql6523776',
+    'database_password': 'qlveS2EJ4h',
+    'database_name': 'sql6523776'
 }
